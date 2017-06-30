bf476b4d7862f8b12e160f0bb7ae00a6b68ebff1
Song Jin <songjin@hotmail.com>
2017-06-30 21:47:19 +1000

    123

--- git_backup_with_cron.sh
+++ git_backup_with_cron.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 COMMIT_MSG=123
+echo 'adding all files to commit'
 git add .
-git commit -m COMMIT_MSG
+echo 'committing...'
+echo 'commit messege is: ' + $COMMIT_MSG
+git commit -m $COMMIT_MSG
+echo 'pushing to master branch'
 git push origin master
