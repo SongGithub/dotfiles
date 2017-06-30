f64029df98ee8a9c9f74b5b8abe3030c0abe495f
Song Jin <songjin@hotmail.com>
2017-06-30 22:04:16 +1000

    daily commit, date time is 10:04:16 pm 06-30-17

--- git_backup_with_cron.sh
+++ git_backup_with_cron.sh
@@ -4,14 +4,20 @@ COMMIT_LEAD_MSG='daily commit, date time is'
 DATE=$(date +"%m-%d-%y")
 TIME=$(date +"%r")
 
-COMMIT_MSG=$COMMIT_LEAD_MSG$TIME$DATE
+COMMIT_MSG=$COMMIT_LEAD_MSG' '$TIME' '$DATE
 echo 'adding all files to commit...'
-git add .
-echo 'committing...'
 echo '\n'
+git add .
+
+
+echo 'committing...'
 echo $COMMIT_MSG
 ECHO_COMMIT_MSG="commit messege is:$COMMIT_MSG"
 echo $ECHO_COMMIT_MSG
 git commit -m "$COMMIT_MSG"
+echo '\n'
+
+
 echo 'pushing to master branch'
 git push origin master
+echo '\n'
